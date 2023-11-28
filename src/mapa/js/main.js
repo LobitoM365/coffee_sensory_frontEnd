ca383a7afdd61c933b787a2ac2d78d7c24a81cb9
LobitoM365 <almariojuancho@gmail.com>
2023-11-27 21:47:51 -0500

    add resize to municipality map

--- src/mapa/js/main.js
+++ src/mapa/js/main.js
@@ -8,6 +8,8 @@ window.addEventListener("load", function () {
     let directionContent;
     let heightContent;
     let predominate = "Width";
+    let municipios = document.querySelectorAll(".municipio-departamento");
+    let departamentoFocus = "";
 
     if (window.innerHeight + 76 >= window.screen.availHeight) {
         heightContent = window.screen.availHeight - 76;
@@ -51,10 +53,49 @@ window.addEventListener("load", function () {
             }
             directionContent = document.body["scroll" + element];
             zoomDivMapa = (directionContent / divMapa["scroll" + element]) * 100;
-            if ( directionContent <= directionContentInicial ) {
+            if (directionContent <= directionContentInicial) {
                 divMapa.style.zoom = zoomDivMapa + "%"
             }
         })
+    }
+
+    function resizeContentToElementFocus(bboxElement, type) {
+        //Saber que tipo de elemento es para restarle a la escala
+        let restScale = 0.5;
+        let dividScale = 2.5;
+        if (type == "departamento") {
+            restScale = 0.5
+            dividScale = 3
+        } else {
+            dividScale = 0;
+            restScale = 2
+        }
+        console.log(restScale)
+        //Calcular la posicion en top y left que movera el contenedor del mapa centrando el elemento al que se le dio click
+        let top = ((bboxElement.y / svgMapa.scrollHeight) * 100) + (((bboxElement.height / 2) / svgMapa.scrollHeight) * 100);
+        let left = ((bboxElement.x / svgMapa.scrollWidth) * 100) + (((bboxElement.width / 2) / svgMapa.scrollWidth) * 100);
+        console.log(top, left, divContentMapa.scrollHeight, ((bboxElement.y / svgMapa.scrollHeight) * 100) + (((bboxElement.height / 2) / svgMapa.scrollHeight)))
+        contentMapa.style.top = "calc(50% + " + top * -1 + "%)";
+        contentMapa.style.left = "calc(50% + " + -left + "% )";
+
+        ///Hacer grande el elemento en relacion a su padre
+        let scale = svgMapa.scrollHeight / bboxElement.height;
+        if (document.body.scrollWidth > document.body.scrollHeight) {
+            
+            if (bboxElement.width > bboxElement.height + (bboxElement.height * 1.2)  ) {
+                scale = ((svgMapa.scrollWidth * (document.body.scrollWidth / contentMapa.scrollWidth)) / bboxElement.width) - restScale;
+            } else {
+                scale = ((svgMapa.scrollHeight * (document.body.scrollHeight / contentMapa.scrollHeight)) / bboxElement.height - restScale);
+            }
+        } else if (document.body.scrollHeight > document.body.scrollWidth) {
+
+            scale = ((svgMapa.scrollWidth * (document.body.scrollWidth / contentMapa.scrollWidth)) / bboxElement.width);
+
+        } else {
+
+            scale = scale - restScale;
+        }
+        divContentMapa.style.transform = "scale(" + scale + ")";
     }
 
     getSizeMap(predominate)
@@ -68,46 +109,56 @@ window.addEventListener("load", function () {
     divContentMapa.style.width = contentMapa.scrollWidth + "px";
     divContentMapa.style.height = contentMapa.scrollHeight + "px";
     for (let x = 0; x < departamentos.length; x++) {
-        let bboxDepartamento = departamentos[x].getBBox();
 
         departamentos[x].addEventListener("click", function () {
+            let bboxDepartamento = departamentos[x].getBBox();
+            let classListDepartamentos = departamentos[x].classList;
+            departamentoFocus = classListDepartamentos[2];
 
-            let top = ((bboxDepartamento.y / svgMapa.scrollHeight) * 100) + (((bboxDepartamento.height / 2) / svgMapa.scrollHeight) * 100);
-            let left = ((bboxDepartamento.x / svgMapa.scrollWidth) * 100) + (((bboxDepartamento.width / 2) / svgMapa.scrollWidth) * 100);
-            console.log(top, left, divContentMapa.scrollHeight, ((bboxDepartamento.y / svgMapa.scrollHeight) * 100) + (((bboxDepartamento.height / 2) / svgMapa.scrollHeight)))
-            contentMapa.style.top = "calc(50% + " + top * -1 + "%)";
-            contentMapa.style.left = "calc(50% + " + -left + "% )";
-
-            ///Hacer grande el departamento en relacion a su padre
-            let scale = svgMapa.scrollHeight / bboxDepartamento.height;
-
-
-
-
-            if (document.body.scrollWidth > document.body.scrollHeight) {
-                if (bboxDepartamento.width > bboxDepartamento.height + (bboxDepartamento.height  )) {
-                    scale = ((svgMapa.scrollWidth * (document.body.scrollWidth / contentMapa.scrollWidth)) / bboxDepartamento.width) - 0.5;
-                } else {
-                    scale = ((svgMapa.scrollHeight * (document.body.scrollHeight / contentMapa.scrollHeight)) / bboxDepartamento.height - 0.5);
-                }
-            } else if (document.body.scrollHeight > document.body.scrollWidth) {
-
-                    scale = ((svgMapa.scrollWidth * (document.body.scrollWidth / contentMapa.scrollWidth)) / bboxDepartamento.width );
-                
-            } else {
-
-                scale = scale - 0.2;
-            }
-            divContentMapa.style.transform = "scale(" + scale + ")";
-
+            resizeContentToElementFocus(bboxDepartamento, "departamento")
             ///Dar focus al elemento seleccionado con su contenido y darle transparencia a los demas
             for (let u = 0; u < departamentos.length; u++) {
-                if (u == x) {
-                    departamentos[x].style.fill = "white"
+                if (x == u) {
+                    departamentos[x].style.fill = "transparent"
+                    departamentos[x].style.visibility = "hidden"
                 } else {
-                    departamentos[u].style.fill = "rgb(255,255,255, 0.4)"
-                }
+                    departamentos[u].style.fill = "rgb(255,255,255, 0.15)"
+                    departamentos[u].style.visibility = ""
 
+                }
+            }
+
+            ///Hacer visible los municipios pertenecientes al departamento
+            for (let m = 0; m < municipios.length; m++) {
+                if (municipios[m].classList.contains(`municipio-${classListDepartamentos[2]}`)) {
+                    municipios[m].style.visibility = "visible"
+                    municipios[m].style.opacity = ""
+
+                } else {
+                    municipios[m].style.visibility = "hidden"
+                }
+            }
+            console.log(municipios, departamentoFocus)
+        })
+    }
+    for (let m = 0; m < municipios.length; m++) {
+
+        municipios[m].addEventListener("click", function () {
+            console.log(departamentoFocus)
+            if (municipios[m].classList.contains(`municipio-${departamentoFocus}`)) {
+                let bboxmunicipio = municipios[m].getBBox();
+                resizeContentToElementFocus(bboxmunicipio, "municipio")
+
+                for (let mf = 0; mf < municipios.length; mf++) {
+
+                    if (mf == m) {
+                        municipios[m].style.opacity = ""
+                    } else {
+                        municipios[mf].style.opacity = "0.3"
+
+                    }
+
+                }
             }
         })
     }
@@ -116,7 +167,6 @@ window.addEventListener("load", function () {
         divContentMapa.style.width = contentMapa.scrollWidth + "px";
         divContentMapa.style.height = contentMapa.scrollHeight + "px";
     })
-
 
 
 

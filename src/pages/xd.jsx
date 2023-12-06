3ad2c22f43b9a172846403b7ca6db84f2803a2a9
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2023-12-06 01:06:40 -0500

    image dafault estate added

--- src/pages/xd.jsx
+++ src/pages/xd.jsx
@@ -30,23 +30,25 @@ export const ModalFinca = () => {
                                 </div>
                             </div>
                             <div className="content">
-
-                                <b>Datos de la finca:</b>
+                                <div className="name-estate">
+                                    <b>Nombre de la finca:</b>
+                                    <div>Villa del prado</div>
+                                </div>
                                 <div className="container-data-user">
                                     <div className="container-profile">
-                                        <img src="public/img/default_profile.jpg" alt="" />
+                                        <img src="public/img/default_estate.png" alt="" />
                                     </div>
                                     <div className="items-data">
                                         <b>Municipio:</b>
-                                        <p>Fabian Ramos</p>
-                                        <b>Vereda:</b>
+                                        <p>Pitalito</p>
+                                        <b>Logitud:</b>
                                         <p>3207851237</p>
                                     </div>
                                     <div className="items-data">
-                                        <b>Apellido:</b>
-                                        <p>Ramos Galindez</p>
-                                        <b>Correo eléctronico:</b>
-                                        <p>fabian@gmail.com</p>
+                                        <b>Vereda:</b>
+                                        <p>Versalles</p>
+                                        <b>Latitud:</b>
+                                        <p>-123112</p>
                                     </div>
                                 </div>
                             </div>
@@ -54,7 +56,7 @@ export const ModalFinca = () => {
                         <div className="container-grafic">
                         </div>
                     </div>
-
+                    {/* Slider */}
                     <div className="container-slider">
                         <div className="container-arrow">
                             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6">

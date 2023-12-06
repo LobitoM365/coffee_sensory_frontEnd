9b1e50b3dc3307e3c886e2aff337cf2c69c594ca
luiscar05 <penaabellaluiscarlos@gmail.com>
2023-12-06 10:45:25 -0500

    pueba

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Formulario } from '../componentes/formulario'
+
 
 
 export const Home = () => {
     const hola = "hola"
     return (
         <>
-            <Formulario/>
+           
         </>
     )
 }

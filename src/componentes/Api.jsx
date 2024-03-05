f0d0213cbfbbe9600c77e2f36422fa622cb1eda1
jfabian04 <ramosgalindezjerlinsonfabian@gmail.com>
2024-03-05 11:36:43 -0500

    se modficó la ip de un MANERA PODEROSA

--- src/componentes/Api.jsx
+++ src/componentes/Api.jsx
@@ -1,6 +1,10 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL : "http://localhost:3000/api",
+    baseURL : "http://10.193.129.44:3000/api",
+    // baseURL : "http://localhost:3000/api",
     withCredentials : true
 }) 
+
+export const host = '10.193.129.44';
+// export const host = 'localhost';

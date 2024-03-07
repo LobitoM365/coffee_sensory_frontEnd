2a83b525888c82ce3ddd010151accb79a601e0b7
luiscar05 <penaabellaluiscarlos@gmail.com>
2024-03-06 23:17:08 -0500

    correccion select

--- src/componentes/Api.jsx
+++ src/componentes/Api.jsx
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-   /*  baseURL : "http://10.193.129.44:3000/api", */
-    baseURL : "http://localhost:3000/api",
+    baseURL : "http://10.193.129.44:3000/api", 
+    /* baseURL : "http://localhost:3000/api", */
     withCredentials : true
 }) 
 

7fdcd3a194812bb7ea8feecaeccb54b525ae2f1b
luiscar05 <penaabellaluiscarlos@gmail.com>
2024-03-13 11:36:59 -0500

    arreglo select y graficos

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -4,7 +4,7 @@ import { GraficoCircular } from '../componentes/GraficoCircular';
 import { SelectComponent } from '../componentes/SelectComponent';
 import Api from '../componentes/Api';
 /* import SinGraficosSVG from '../assets/svg/SinGraficos.svg'; */
-
+import Logo from '../assets/SinGraficos.svg';
 
 export const Home = ({ userInfo }) => {
 
@@ -25,35 +25,32 @@ export const Home = ({ userInfo }) => {
  
   const [urlIdReady, setUrlIdReady] = useState(false);
   const [muestrasId, setMuestrasId] = useState(null);
-  
-  
+  const [fechas,setFechas]=useState([])
+  const [fechas2,setFechas2]=useState([])
   
   useEffect(() => {
     setUser(userInfo);
   }, [userInfo]);
 
+
   useEffect(() => {
     const IdUsuario = async () => {
       if (user?.id !== null && user?.id !== undefined) {
-
-          const response= await Api.post(`analisis/total/${user.id}`,inputValue)
-
-          setData(response.data.data)
-          SetUrlData(true)
-          console.log(Data,"hola no tienes registros disponibles")
-
-          setUrl(`/analisis/total/${user.id}`);
-          
-          setMuestrasId(inputValue.muestras_id); // Establece muestrasId
-          setUrlIdReady(true);
-          console.log(muestrasId,"opcion 2")
-        
-       
+        const response = await Api.post(`analisis/total/${user.id}`, inputValue);
+  
+        setData(response.data.data);
+        SetUrlData(true);
+        console.log(Data, "hola no tienes registros disponibles");
+  
+        setUrl(`/analisis/total/${user.id}`,inputValue);
+        setMuestrasId(inputValue.muestras_id); // Establece muestrasId
+        setUrlIdReady(true);
+        console.log(muestrasId, "opcion 2");
       }
     };
   
     IdUsuario();
-  }, [user, inputValue.muestras_id,]);
+  }, [user, inputValue.muestras_id]);
   
 
   useEffect(() => {
@@ -84,21 +81,105 @@ export const Home = ({ userInfo }) => {
   console.log(inputValue,"valores del input")
   // Actualiza la URL de SelectComponent cuando cambia el usuario
   
+  console.log(Data,"sin datos")
 
+  useEffect(() => {
+    if (inputValue.muestras_id) {
+      // Llamada a la API para obtener las fechas asociadas al muestras_id
+      const obtenerFechas = async () => {
+        try {
+         
+          const response = await Api.post(
+            `/analisis/total/${user.id}`,inputValue
+          );
+          console.log(response,"si se consume")
   
-  if (urlData && Data.length == 0) {
-    console.log(Data.length,"aqui va un componente")
+          if (response.data.data && response.data.data.length > 0) {
+            // Mapear los datos de fechas para el SelectComponent
+            const nuevasFechas = response.data.data.map((fecha) => ({
+              value: fecha.id,
+              label: fecha.fecha,
+            }));
+  
+            // Actualizar el estado para que SelectComponent muestre las nuevas fechas
+            setFechas(nuevasFechas);
+          } else {
+            console.error("No se encontraron fechas válidas para el muestras_id");
+          }
+        } catch (error) {
+          console.error("Error al obtener fechas:", error.message);
+        }
+      };
+  
+      obtenerFechas();
+    }else{
+      const obtenerFechas = async () => {
+
+        
+        try {
+          
+            const response = await Api.post(`/analisis/total/${user.id}`, inputValue);
+        
+            if (response.data.data && response.data.data.length > 0) {
+              // Mapear los datos de fechas para el SelectComponent
+              const nuevasFechas = response.data.data.map((fecha) => ({
+                value: fecha.id,
+                label: fecha.fecha,
+              }));
+              
+              console.log(nuevasFechas, "fechas nuevas sin datos");
+              
+              // Actualizar el estado para que SelectComponent muestre las nuevas fechas
+              setFechas2(nuevasFechas);
+              
+            } else {
+              console.error("No se encontraron fechas válidas para el muestras_id");
+              // Puedes manejar este caso actualizando el estado con un valor predeterminado
+              setFechas2([{ value: '0', label: 'Sin fechas disponibles' }]);
+            }
+          
+        } catch (error) {
+          console.error("Error al obtener fechas:", error.message);
+          // Puedes manejar este caso actualizando el estado con un valor predeterminado
+          setFechas2([{ value: '', label: 'Error al cargar fechas' }]);
+        }
+      };
+
+      
+      obtenerFechas();
+      console.log(fechas,"fechas")
+    }
+
+  }, [inputValue.muestras_id]);
+  useEffect(() => {
+    const cargarFechas = async () => {
+      try {
+        if (urlIdReady) {
+          const response = await Api.post(urlId)
+          const nuevasFechas = response.data.data.map((item) => item.fecha);
+          setFechas(nuevasFechas);
+        }
+      } catch (error) {
+        console.error('Error al cargar las fechas:', error.message);
+      }
+    };
+
+    cargarFechas();
+  }, [urlId, urlIdReady]);
+  
+  if (urlData && Data == undefined || Data == []) {
+    console.log(Data,"aqui va un componente")
     return(
       <>
         <link rel="stylesheet" href="src/css/graficas.css" />
         <link rel="stylesheet" href="../../public/css/graficos.css" />
-        <div id='graficos' className="BoxMain">
-          <div className='BoxGraficas'>
-            
-          </div>
-          <div className='Tablecolors'>
-              <H3>Lo Siento en este momento no tienes graficas disponibles</H3>
-          </div>
+        <div id='graficos' >
+          {/* <div className='BoxGraficas'> */}
+            <img src={Logo} className="SinGraficos2"  alt="No Hay Graficas" />
+          {/* </div> */}
+          {/* <div className='Tablecolors'> */}
+              <h3 className="text-Graphic-none">Lo Siento en este momento no tienes graficas disponibles </h3>
+         {/*  </div> */}
         </div>
       </>
     )
@@ -108,7 +189,7 @@ export const Home = ({ userInfo }) => {
         <link rel="stylesheet" href="src/css/graficas.css" />
         <link rel="stylesheet" href="../../public/css/graficos.css" />
   
-        <div id='graficos' className="BoxMain">
+        <div  className="BoxMain">
           <div  className="BoxGraficas">
              
             <div className="formulario">
@@ -127,7 +208,8 @@ export const Home = ({ userInfo }) => {
             }
             url={urlId}
             opcion="fecha"
-           
+            options={fechas.length !== 0 ? fechas : fechas2}
+
           />
         )}
               <SelectComponent
